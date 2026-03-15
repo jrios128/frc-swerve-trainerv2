49dc80f4818ac8182e82a45806033dd2eb216d70
jrios128 <52140306+jrios128@users.noreply.github.com>
2026-03-15 14:02:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const canvas = document.getElementById("field");
 const ctx = canvas.getContext("2d");
 
@@ -86,31 +85,3 @@ requestAnimationFrame(loop);
 }
 
 loop();
-
-let time = 120;
-let interval;
-
-function startTimer(){
-
-clearInterval(interval);
-
-interval = setInterval(()=>{
-
-time--;
-let min = Math.floor(time/60);
-let sec = time%60;
-
-document.getElementById("timer").innerText =
-`${min}:${sec.toString().padStart(2,'0')}`;
-
-if(time<=0) clearInterval(interval);
-
-},1000);
-
-}
-
-function resetTimer(){
-clearInterval(interval);
-time = 120;
-document.getElementById("timer").innerText="2:00";
-}
