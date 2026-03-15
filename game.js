84cea920e72e86ecb87ea29af538275749a70636
jrios128 <52140306+jrios128@users.noreply.github.com>
2026-03-15 13:37:34 -0500

    Create game.js
    Added game score

---
+++ game.js
@@ -0,0 +1,22 @@
+function checkTargets(){
+
+targets.forEach(t=>{
+
+let dx = robot.x - t.x;
+let dy = robot.y - t.y;
+
+let dist = Math.sqrt(dx*dx+dy*dy);
+
+if(dist < t.r){
+
+score += 5;
+
+}
+
+});
+
+}
+
+ctx.fillStyle="white";
+ctx.font="24px Arial";
+ctx.fillText("Score: "+score,20,30);
