cb6c67c4b55075b43bfbf450254431d5c4301421
jrios128 <52140306+jrios128@users.noreply.github.com>
2026-03-15 13:58:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -86,3 +86,31 @@ requestAnimationFrame(loop);
 }
 
 loop();
+
+let time = 120;
+let interval;
+
+function startTimer(){
+
+clearInterval(interval);
+
+interval = setInterval(()=>{
+
+time--;
+let min = Math.floor(time/60);
+let sec = time%60;
+
+document.getElementById("timer").innerText =
+`${min}:${sec.toString().padStart(2,'0')}`;
+
+if(time<=0) clearInterval(interval);
+
+},1000);
+
+}
+
+function resetTimer(){
+clearInterval(interval);
+time = 120;
+document.getElementById("timer").innerText="2:00";
+}
