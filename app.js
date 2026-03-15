05f4ba77d9e01ed5ad9e4508a32c25cd6902f2c3
jrios128 <52140306+jrios128@users.noreply.github.com>
2026-03-15 15:09:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,34 @@ let keys = {};
 document.addEventListener("keydown", e => keys[e.key]=true);
 document.addEventListener("keyup", e => keys[e.key]=false);
 
+let time = 120;
+let interval;
+
+function startTimer(){
+
+clearInterval(interval);
+
+interval = setInterval(()=>{
+
+time--;
+let min = Math.floor(time/60);
+let sec = time%60;
+
+document.getElementById("timer").innerText =
+`${min}:${sec.toString().padStart(2,'0')}`;
+
+if(time<=0) clearInterval(interval);
+
+},1000);
+
+}
+
+function resetTimer(){
+clearInterval(interval);
+time = 120;
+document.getElementById("timer").innerText="2:00";
+}
+
 function update(){
 
 let speed = 3;
