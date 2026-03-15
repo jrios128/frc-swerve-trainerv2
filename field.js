1cea74105d3fb711776834d1032eaac3e0b4160d
jrios128 <52140306+jrios128@users.noreply.github.com>
2026-03-15 14:38:18 -0500

    Create field.js

---
+++ field.js
@@ -0,0 +1,30 @@
+function drawField(ctx, canvas){
+
+ctx.fillStyle="#0b5e20";
+ctx.fillRect(0,0,canvas.width,canvas.height);
+
+ctx.strokeStyle="white";
+ctx.lineWidth=4;
+
+ctx.strokeRect(20,20,canvas.width-40,canvas.height-40);
+
+ctx.beginPath();
+ctx.moveTo(canvas.width/2,20);
+ctx.lineTo(canvas.width/2,canvas.height-20);
+ctx.stroke();
+
+drawObstacle(ctx,200,200);
+drawObstacle(ctx,500,350);
+drawObstacle(ctx,650,150);
+
+}
+
+function drawObstacle(ctx,x,y){
+
+ctx.fillStyle="#777";
+ctx.fillRect(x,y,60,60);
+
+ctx.strokeStyle="black";
+ctx.strokeRect(x,y,60,60);
+
+}
