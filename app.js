8bdb4a857992bc2f777b16bc87208e8cd2273e0e
jrios128 <52140306+jrios128@users.noreply.github.com>
2026-03-15 15:15:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -110,6 +110,28 @@ ctx.fillRect(500,350,80,80);
 
 }
 
+function drawField(ctx, canvas){
+
+ctx.fillStyle="#0b5e20";
+ctx.fillRect(0,0,canvas.width,canvas.height);
+
+ctx.strokeStyle="white";
+ctx.lineWidth=4;
+
+ctx.strokeRect(20,20,canvas.width-40,canvas.height-40);
+
+// center line
+ctx.beginPath();
+ctx.moveTo(canvas.width/2,20);
+ctx.lineTo(canvas.width/2,canvas.height-20);
+ctx.stroke();
+
+// scoring hubs
+drawTarget(ctx,150,250);
+drawTarget(ctx,750,250);
+
+}
+
 function loop(){
 update();
 draw();
