825a2f453dd48ec0a523767c224baa1f49016ba2
jrios128 <52140306+jrios128@users.noreply.github.com>
2026-03-15 16:42:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,33 @@ robot.y += dy;
 }
 
 }
+let time = 120;
+let interval;
 
+function startTimer(){
+
+clearInterval(interval);
+
+interval = setInterval(()=>{
+
+time--;
+let min = Math.floor(time/60);
+let sec = time%60;
+
+document.getElementById("timer").innerText =
+`${min}:${sec.toString().padStart(2,'0')}`;
+
+if(time<=0) clearInterval(interval);
+
+},1000);
+
+}
+
+function resetTimer(){
+clearInterval(interval);
+time = 120;
+document.getElementById("timer").innerText="2:00";
+}
 function readGamepad(inputX,inputY){
 
 let gamepads = navigator.getGamepads();
