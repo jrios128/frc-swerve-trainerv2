32b8832182393bc7c34460d7a6c4fb10681565b9
jrios128 <52140306+jrios128@users.noreply.github.com>
2026-03-15 13:27:11 -0500

    Create aiBots.js
    Added Defense bots

---
+++ aiBots.js
@@ -0,0 +1,53 @@
+let defenders = [
+{x:300,y:150,angle:0},
+{x:600,y:350,angle:0}
+];
+
+function updateDefenders(){
+
+defenders.forEach(bot=>{
+
+let dx = robot.x - bot.x;
+let dy = robot.y - bot.y;
+
+bot.x += dx * 0.01;
+bot.y += dy * 0.01;
+
+});
+
+}
+function drawDefenders(ctx){
+
+ctx.fillStyle="red";
+
+defenders.forEach(bot=>{
+
+ctx.save();
+
+ctx.translate(bot.x,bot.y);
+ctx.fillRect(-20,-20,40,40);
+
+ctx.restore();
+
+});
+
+}
+function checkCollisions(){
+
+defenders.forEach(bot=>{
+
+let dx = robot.x - bot.x;
+let dy = robot.y - bot.y;
+
+let dist = Math.sqrt(dx*dx+dy*dy);
+
+if(dist < 40){
+
+robot.vx *= -0.5;
+robot.vy *= -0.5;
+
+}
+
+});
+
+}
