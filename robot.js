90b527b7aa6e7828d4342e88020a5d447b7bc500
jrios128 <52140306+jrios128@users.noreply.github.com>
2026-03-15 21:41:25 -0500

    Update robot.js

--- robot.js
+++ robot.js
@@ -1,1 +1,52 @@
 
+let robot = {
+x:450,
+y:250,
+angle:0,
+vx:0,
+vy:0,
+size:50
+};
+
+let swerveModules = [
+{x:-20,y:-20,angle:0},
+{x:20,y:-20,angle:0},
+{x:-20,y:20,angle:0},
+{x:20,y:20,angle:0}
+];
+
+function drawRobot(ctx){
+
+ctx.save();
+ctx.translate(robot.x,robot.y);
+ctx.rotate(robot.angle);
+
+ctx.fillStyle="orange";
+ctx.fillRect(-robot.size/2,-robot.size/2,robot.size,robot.size);
+
+drawSwerveModules(ctx);
+
+ctx.restore();
+
+}
+
+function drawSwerveModules(ctx){
+
+ctx.strokeStyle="cyan";
+
+swerveModules.forEach(m=>{
+
+ctx.save();
+ctx.translate(m.x,m.y);
+ctx.rotate(m.angle);
+
+ctx.beginPath();
+ctx.moveTo(0,0);
+ctx.lineTo(15,0);
+ctx.stroke();
+
+ctx.restore();
+
+});
+
+}
