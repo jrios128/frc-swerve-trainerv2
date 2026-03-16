851772544961c46de56144220dfb8c98306e297b
jrios128 <52140306+jrios128@users.noreply.github.com>
2026-03-15 21:53:54 -0500

    Update robot.js

--- robot.js
+++ robot.js
@@ -1,4 +1,3 @@
-
 let robot = {
 x:450,
 y:250,
@@ -6,47 +5,61 @@ angle:0,
 vx:0,
 vy:0,
 size:50
-};
+}
 
 let swerveModules = [
 {x:-20,y:-20,angle:0},
 {x:20,y:-20,angle:0},
 {x:-20,y:20,angle:0},
 {x:20,y:20,angle:0}
-];
+]
+
+function updateRobot(){
+
+robot.x += robot.vx
+robot.y += robot.vy
+
+robot.vx *= 0.9
+robot.vy *= 0.9
+
+}
 
 function drawRobot(ctx){
 
-ctx.save();
-ctx.translate(robot.x,robot.y);
-ctx.rotate(robot.angle);
+ctx.save()
 
-ctx.fillStyle="orange";
-ctx.fillRect(-robot.size/2,-robot.size/2,robot.size,robot.size);
+ctx.translate(robot.x,robot.y)
 
-drawSwerveModules(ctx);
+ctx.rotate(robot.angle)
 
-ctx.restore();
+ctx.fillStyle="orange"
+ctx.fillRect(-robot.size/2,-robot.size/2,robot.size,robot.size)
+
+drawSwerveModules(ctx)
+
+ctx.restore()
 
 }
 
 function drawSwerveModules(ctx){
 
-ctx.strokeStyle="cyan";
+ctx.strokeStyle="cyan"
 
 swerveModules.forEach(m=>{
 
-ctx.save();
-ctx.translate(m.x,m.y);
-ctx.rotate(m.angle);
+ctx.save()
 
-ctx.beginPath();
-ctx.moveTo(0,0);
-ctx.lineTo(15,0);
-ctx.stroke();
+ctx.translate(m.x,m.y)
 
-ctx.restore();
+ctx.rotate(m.angle)
 
-});
+ctx.beginPath()
+ctx.moveTo(0,0)
+ctx.lineTo(15,0)
+ctx.stroke()
+
+ctx.restore()
+
+})
 
 }
