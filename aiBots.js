2361bac6b1796cc2e2b725f9492e11137a956f25
jrios128 <52140306+jrios128@users.noreply.github.com>
2026-03-15 21:55:02 -0500

    Update aiBots.js

--- aiBots.js
+++ aiBots.js
@@ -1,36 +1,39 @@
 let defenders = [
 {x:300,y:200},
 {x:650,y:300}
-];
+]
 
 function updateDefenders(){
 
 defenders.forEach(bot=>{
 
-let futureX = robot.x + robot.vx * 30;
-let futureY = robot.y + robot.vy * 30;
+let futureX = robot.x + robot.vx * 30
+let futureY = robot.y + robot.vy * 30
 
-let dx = futureX - bot.x;
-let dy = futureY - bot.y;
+let dx = futureX - bot.x
+let dy = futureY - bot.y
 
-bot.x += dx * 0.02;
-bot.y += dy * 0.02;
+bot.x += dx * 0.02
+bot.y += dy * 0.02
 
-});
+})
 
 }
 
 function drawDefenders(ctx){
 
-ctx.fillStyle="red";
+ctx.fillStyle="red"
 
 defenders.forEach(bot=>{
 
-ctx.save();
-ctx.translate(bot.x,bot.y);
-ctx.fillRect(-20,-20,40,40);
-ctx.restore();
+ctx.save()
 
-});
+ctx.translate(bot.x,bot.y)
+
+ctx.fillRect(-20,-20,40,40)
+
+ctx.restore()
+
+})
 
 }
