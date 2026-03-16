93327231814778d3f29e012b6349d16a000781dd
jrios128 <52140306+jrios128@users.noreply.github.com>
2026-03-15 22:34:20 -0500

    Replacing field layout with updated boundaries

--- field.js
+++ field.js
@@ -1,16 +1,38 @@
 function drawField(ctx,canvas){
 
+const margin = 20
+
+/* FIELD FLOOR */
+
 ctx.fillStyle="#0b5e20"
 ctx.fillRect(0,0,canvas.width,canvas.height)
 
+/* FIELD BORDER */
+
 ctx.strokeStyle="white"
 ctx.lineWidth=4
+ctx.strokeRect(margin,margin,canvas.width-margin*2,canvas.height-margin*2)
 
-ctx.strokeRect(20,20,canvas.width-40,canvas.height-40)
+/* CENTER LINE */
 
 ctx.beginPath()
-ctx.moveTo(canvas.width/2,20)
-ctx.lineTo(canvas.width/2,canvas.height-20)
+ctx.moveTo(canvas.width/2,margin)
+ctx.lineTo(canvas.width/2,canvas.height-margin)
 ctx.stroke()
+
+/* SCORING ZONES */
+
+drawScoringZone(80,200,"blue")
+drawScoringZone(canvas.width-140,200,"red")
+
+/* LOADING ZONES */
+
+drawLoadingZone(80,80,"blue")
+drawLoadingZone(canvas.width-140,canvas.height-140,"red")
+
+/* MIDFIELD OBSTACLES */
+
+drawObstacle(canvas.width/2-40,180)
+drawObstacle(canvas.width/2+20,300)
 
 }
